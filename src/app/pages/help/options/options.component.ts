2b6d1a0647c912961752f7b789c2ba74915bd23a
“Young <hogusong1@gmail.com>
2019-08-09 19:03:29 -0400

    Add all functions to update Game Configuration and connect it to Menu.

--- src/app/pages/help/options/options.component.ts
+++ src/app/pages/help/options/options.component.ts
@@ -1,28 +1,77 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { BaseService } from 'src/app/providers/base.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-options',
   templateUrl: './options.component.html',
   styleUrls: ['./options.component.css']
 })
-export class OptionsComponent implements OnInit {
+export class OptionsComponent implements OnInit, OnDestroy {
+
   @Input() UIC: any;
-  
+  message: string = '';
+  myForm: FormGroup;
+  timer: any;
+
   constructor(private baseService: BaseService) { }
 
-  ngOnInit() {
+  ngOnInit() { 
+    this.setInitialForm();
   }
 
+  // Set auto-save whenever value has been changed.
   valueChanged() {
-
+    if (this.myForm.valid) {
+      if (this.myForm.value.autoPlay === 'Yes') {
+        this.myForm.controls['setPlayer'].setValue('in play');
+      }
+      const config = {
+        showResult: this.myForm.value.howToShow === 'inBox',
+        delay: this.myForm.value.delayTime * 1000,
+        delay4dealer: this.myForm.value.delay4dealer * 1000,
+        keepInPlay: this.myForm.value.setPlayer === 'in play',
+        keepLastBet: this.myForm.value.keepBet === 'keep',
+        minBetting: this.myForm.value.minBet,
+        howManyDecks: this.myForm.value.decks,
+        autoPlay: this.myForm.value.autoPlay === 'Yes',  
+      }
+      this.baseService.setConfig(config);
+      this.message = 'New options are saved with changes.';
+      this.timer = setTimeout(() => this.message = '', 1500);
+    }
   }
-  
-  resetDefaultValue() {
 
+  resetDefaultValue() {
+    this.message = 'Reset Defult options.';
+    this.timer = setTimeout(() => this.message = '', 1500);
+    this.baseService.resetDefultConfig();
+    this.setInitialForm();
+  }
+
+  setInitialForm() {
+    const config = this.baseService.getConfig();
+    this.myForm = new FormGroup({
+      howToShow: new FormControl(config.showResult ? 'inBox' : 'delay'),
+      delayTime: new FormControl(config.delay/1000, 
+        {validators: [Validators.required, Validators.min(1), Validators.max(7)]}),
+      delay4dealer: new FormControl(config.delay4dealer/1000,
+        {validators: [Validators.required, Validators.min(1), Validators.max(7)]}),
+      setPlayer: new FormControl(config.keepInPlay ? 'in play' : 'stay out'),
+      keepBet: new FormControl(config.keepLastBet ? 'keep' : 'reset'),
+      minBet: new FormControl(config.minBetting, 
+        {validators: [Validators.required, Validators.min(5)]}),
+      decks: new FormControl(config.howManyDecks, 
+        {validators: [Validators.required, Validators.min(4), Validators.max(10)]}),
+      autoPlay: new FormControl(config.autoPlay ? 'Yes' : 'No')
+    });
   }
 
   backToPreviousUI() {
     this.baseService.setUIconfig(this.UIC);
+  }
+
+  ngOnDestroy() {
+    clearTimeout(this.timer);
   }
 }
--- src/app/providers/base.service.ts
+++ src/app/providers/base.service.ts
@@ -5,9 +5,16 @@ import { Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class BaseService {
-  private UIconfig: any;
-  private UIconfigSubject = new Subject<any>()
+  private UIconfig: any;        // Keep UI configuration globally.
+  private UIconfigSubject = new Subject<any>();
+  private config: any;          // Keep game configuration globally.
+  private configSubject = new Subject<any>();
+
   constructor() {
+    this.config = JSON.parse(localStorage.getItem('bj-config'));
+    if (!this.config) this.config = this.getDefaultConfig();
+    this.config.delay4dealer = 1000;
+    this.configSubject.next(this.config);
     this.UIconfigSubject.next(this.UIconfig = this.getDefaultUIconfig());
   }
 
@@ -31,4 +38,28 @@ export class BaseService {
   getUIconfig() {  return this.UIconfig;  }
   getUIconfigSubject() {  return this.UIconfigSubject.asObservable();  }
   setUIconfig(config) {  this.UIconfigSubject.next(this.UIconfig = config);  }
+
+  getConfigSubject() {  return this.configSubject.asObservable();  }
+  getConfig() {  return this.config;  }
+  getDefaultConfig() {
+    return {
+      showResult: true,
+      delay: 2000,
+      delay4dealer: 1000,
+      keepInPlay: true,
+      keepLastBet: true,
+      minBetting: 5,
+      howManyDecks: 6,
+      autoPlay: false,
+    }
+  }
+
+  setConfig(config) {
+    localStorage.setItem('bj-config', JSON.stringify(config));
+    this.configSubject.next(this.config = config);
+  }
+
+  resetDefultConfig() {
+    this.setConfig(this.getDefaultConfig());
+  }
 }
