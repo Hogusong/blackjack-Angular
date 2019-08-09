66a875611f450ba2eea704c0920f3e0092a68852
“Young <hogusong1@gmail.com>
2019-08-09 16:05:48 -0400

    Create 'CardService' to provide all activities regarding cards.

---
+++ src/app/providers/card.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import CARD from '../models/card';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CardService {
+  private cards: CARD[];
+
+  constructor() { 
+    this.createCards();
+  }
+
+  createCards() {
+    let cards = [];
+    const noOfDecks = 6;
+    for (let i = 0; i < noOfDecks; i++) {
+      cards = [...cards, ...buildDeck()]
+    }  
+    this.cards = shuffle(cards, noOfDecks);
+  }
+
+  getCards() {
+    return [...this.cards];
+  }
+}
+
+function buildDeck() {
+  const base = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  const suits = ['spades',  'clubs', 'hearts', 'diams'];
+  const deck = [];
+  for (let s of suits) {
+    const keys = base.sort((a, b) => 0.5 - Math.random());
+    for (let k of keys) {
+      deck.push(new CARD(s, k));
+    }
+  }
+  return deck;
+}
+
+function shuffle(cards, noOfDeck) {
+  cards = cards.sort((a, b) => 0.5 - Math.random());
+  const front = cards.slice(0, cards.length/2);
+  const back =  cards.slice(cards.length/2);
+  const count = front.length / noOfDeck;
+  cards = []
+  for (let i = 0; i < noOfDeck; i++) {
+    cards = cards.concat(mixCards( front.slice(i*count, (i+1)*count), back.slice(i*count, (i+1)*count) ))
+  }
+  const cutCount = Math.floor(52 * noOfDeck / 12);
+  return [...cards.splice(-cutCount), ...cards];
+}
+
+function mixCards(A, B) {
+  const result = []
+  for (let i = 0; i < A.length; i++) {
+    result.push(A[i]);
+    result.push(B[i]);
+  }
+  return [...result];
+}
