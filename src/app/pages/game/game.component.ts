5fec392e895f52ec1f56e634e834be5cd7429de7
“Young <hogusong1@gmail.com>
2019-08-09 22:33:28 -0400

    Create 'Split, Double, Hit, and Stay' modules and alert message box.

--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -113,19 +113,99 @@ export class GameComponent implements OnInit {
   }
 
   splitCards(i) {
-    console.log('split', i)
+    // Confirm that the active player clicked the button.
+    if (this.currIndex === i) {
+      const onHand = this.players[i].getOnHand();
+      const v_0 = onHand[0].getValue();
+      const v_1 = onHand[1].getValue();
+      if (onHand.length == 2 && v_0 == v_1) {
+        if (v_0 == 11 && this.players[i].getIsSplited()) {
+          this.errorMessage(`<p>Second SPLIT with double Aces is not allowed!</p>`);
+        } else if (this.players[i].getAmount() < this.players[i].getBetting() * 2) {
+          this.errorMessage(`<p>Sorry! You have not enough money for SPLIT.</p>`);
+        } else {
+          // Remark as 'SPLITED' and replace the hand with only one card.
+          this.players[i].setIsSplited(true);
+          this.players[i].setOnHand([onHand[0]]);
+          // Duplicate player and let player has same one card and remark 
+          const temp = this.playerService.clonePlayer(this.players[i]);
+          temp.setIsSplited(true);
+          temp.setOnHand([onHand[1]]);
+          // Replace the current player and insert this duplicated player to the players list
+          // and the gameResult list in same position(right next of the current player). 
+          const P = this.players;
+          this.players = [...P.slice(0, i), this.players[i], temp, ...P.slice(i+1)];
+          this.gameResult = [...this.gameResult.slice(0,i), 0, 0, ...this.gameResult.slice(i+1)];
+        }
+      } else {
+        this.errorMessage(`
+          <p>Not allowed 'SPLIT'!</p>
+          <p>You have more than 2 cards or not same cards!</p>`);
+      }
+    }
   }
 
   doubleDown(i) {
-    console.log('double down', i)
+    // Confirm that the active player clicked the button.
+    if (this.currIndex === i) {
+      if (this.players[i].getOnHand().length != 2) {
+        this.errorMessage(`<p>DOUBLE DOWN is allowed<br>when player has only 2 cards!</p>`);
+      } else {
+        this.cardService.drawCard(this.players[i]);
+        if (this.players[i].getAmount() < this.players[i].getBetting() * 2) {
+          this.players[i].setBetting(this.players[i].getAmount());          
+          this.players[i].setPrevResult('DOUBLE betting for less. Good luck!');
+        } else {
+          this.players[i].setBetting(this.players[i].getBetting() * 2);
+          this.players[i].setPrevResult('Player made DOUBLE betting. Good luck!');
+        }
+        this.players[i].setCanDraw(false);    // Disable this player. set pending.
+        this.getCurrIndex();                  // Move to the next available player.
+      }
+    }
   }
 
   drawOneMore(i) {
-    console.log('hit', i)
+    // Confirm that the active player clicked the button.
+    if (this.currIndex === i) {
+      this.cardService.drawCard(this.players[i]);
+
+      // If this player had only one card which was 'A', 
+      // let the player stay out as pending status after drew a card.
+      const onHand = this.players[i].getOnHand();
+      if (onHand.length == 2 && onHand[0].getValue() == 11) {
+        this.players[i].setCanDraw(false);
+        this.stayInGame(i);
+      }
+      // If the player's score went over 21, the player loose this hand.
+      if (this.players[i].getScore() > 21) {
+        this.players[i].setPrevResult('Player lost this hand!');
+        this.players[i].looseHand();      // Process as lost and disable this player.
+        this.gameResult[i] = -1;          // Remark as looser in the gameResult list.
+        this.getCurrIndex();              // Move to the next available player.
+      }
+    }
   }
 
   stayInGame(i) {
-    console.log('stay', i)
+    // Confirm that the active player clicked the button.
+    if (this.currIndex === i) {
+      if (this.players[i].getOnHand().length < 2) {
+        this.errorMessage(`
+          <p>Not allowed to stay with one card.<br>You must HIT this time!</p>`)
+      } else {
+        this.players[i].setPrevResult('Player stay with the current score.');
+        this.players[i].setCanDraw(false);    // Disable this player. set pending.
+        this.getCurrIndex();                  // Move to the next available player.
+      }
+    }
+  }
+
+  errorMessage(markup) {
+    this.dialogBox = {
+      open: true,
+      markup: markup
+    }
   }
 
   drawDealerCards() {
